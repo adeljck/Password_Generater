3ae22d6f5d827376eb843eec76c5c7f3de03ed44
adeljck <1612002884@qq.com>
2020-08-07 19:35:29 +0800

    upload to github

---
+++ src/app/pages/404.vue
@@ -0,0 +1,16 @@
+<template>
+    <p>Page not found</p>
+</template>
+
+<script>
+  export default {
+    name:'not_found'
+  }
+</script>
+<style scoped>
+    p{
+        font-size: 36px;
+        margin-top: 20%;
+        text-align: center;
+    }
+</style>
